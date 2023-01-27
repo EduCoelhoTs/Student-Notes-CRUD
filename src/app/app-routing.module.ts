ece4498f40b676cee62411eb56d8b7dfb2c96483
Eduardo Coelho <eduardocoelhosilva12@gmail.com>
2023-01-27 10:27:34 -0300

    Componente nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'students',
+    loadChildren: () => import('./modules/students/students.module').then(m => m.StudentsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
